74a1e8160004b1c70d9bbac989058f8900626d95
Ben <benward151@gmail.com>
2024-05-12 11:49:14 +0200

    error page

---
+++ error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Nav />
+    <Header
+      imageUrl="/images/infrared-room.png"
+      :title="`${error.statusCode} Error`"
+      subtitle="This Page Does Not Exist"
+      headerClass="header-small"
+      buttonText="Go Home"
+      buttonUrl="/"
+      buttonDescription="Go Home">
+      <template #description> <p>I haven't created this page yet.</p> </template>
+    </Header>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+
+useHead({
+  title: "Bath Spa - Error",
+  meta: [{ name: "description", content: "My amazing site." }],
+});
+</script>
