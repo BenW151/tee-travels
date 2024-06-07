26cf3091bff30b21ffa06812918c313ec37b1c1f
Ben <benward151@gmail.com>
2024-06-07 18:02:40 +0300

    added sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from "path";
 export default defineNuxtConfig({
+  site: {
+    url: 'https://whynotadventures.co.uk/',
+    name: 'Why Not Adventures',
+  },
   devtools: { enabled: true },
   alias: {
     assets: "/<rootDir>/assets",
@@ -11,6 +15,7 @@ export default defineNuxtConfig({
     "@nuxtjs/google-fonts",
     "@nuxt/image",
     "nuxt-swiper",
+    "@nuxtjs/sitemap"
   ],
   image: {
     format: ["webp"],
