613dc2324f67c785d71e5ce4d788999b328a4dba
Ben <benward151@gmail.com>
2024-10-06 14:08:29 +0900

    add noindex across entire site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,6 +77,11 @@ export default defineNuxtConfig({
         lang: "en",
       },
       meta: [
+        {
+          hid: "robots",
+          name: "robots",
+          content: "noindex, nofollow"
+        },
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1.0" },
         {
